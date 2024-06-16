cbc268253a2a8d2f8d9fd54d5d5dd63863f1d732
muoscar <oscar.muhr@gmail.com>
2024-06-16 21:58:54 +0200

    feat: added actix-web and a GET and POST endpoint at root, removed DB from version control

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,12 +1,40 @@
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use futures::TryStreamExt;
 use sqlx::{migrate::MigrateDatabase, Connection, Row, Sqlite, SqliteConnection};
 
 const DB_URL: &str = "sqlite://sqlite.db";
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    println!("Hello, world!");
+#[get("/")]
+async fn dump_log() -> impl Responder {
+    let mut conn = SqliteConnection::connect(DB_URL).await.unwrap();
+    let mut messages = sqlx::query("SELECT * FROM messages;").fetch(&mut conn);
 
+    let mut response = Vec::new();
+    while let Some(message) = messages.try_next().await.unwrap() {
+        let id: u32 = message.try_get("id").unwrap();
+        let msg: String = message.try_get("message").unwrap();
+        response.push(format!("{{ \"id\": {id}, \"message\": \"{msg}\" }}"));
+    }
+    let response = response.join(", ");
+    HttpResponse::Ok().body(format!("[ {response} ]"))
+}
+
+#[post("/")]
+async fn send_message(msg: String) -> impl Responder {
+    println!("Message received:\n\t{msg}");
+
+    let mut conn = SqliteConnection::connect(DB_URL).await.unwrap();
+    sqlx::query("INSERT INTO messages (message) VALUES (?)")
+        .bind(msg.clone())
+        .execute(&mut conn)
+        .await
+        .unwrap();
+
+    HttpResponse::Ok().body(msg)
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), sqlx::Error> {
     if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
         println!("Creating database {}", DB_URL);
         match Sqlite::create_database(DB_URL).await {
@@ -39,6 +67,11 @@ async fn main() -> Result<(), sqlx::Error> {
         let msg: String = message.try_get("message")?;
         println!("Message {id}: {msg}");
     }
+
+    let _ = HttpServer::new(|| App::new().service(dump_log).service(send_message))
+        .bind(("0.0.0.0", 32123))?
+        .run()
+        .await;
 
     Ok(())
 }
