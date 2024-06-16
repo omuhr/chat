b996e53cee91563e884b8a77cd16d57d7564ea12
Johan Philis <johan.e.philis@gmail.com>
2024-06-16 15:14:04 +0200

    wip sqlite

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,4 +1,31 @@
+use sqlx::{Connection, SqliteConnection, };
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error> {
     println!("Hello, world!");
+
+    let mut conn = SqliteConnection::connect("sqlite::memory:").await?;
+
+    sqlx::query("BEGIN").execute(&mut conn).await?;
+
+    sqlx::query(
+        "CREATE TABLE messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        message MEDIUMTEXT NOT NULL
+        );"
+        ).execute(&mut conn).await?;
+
+    sqlx::query(
+        "INSERT INTO messages (message) VALUES (\"text\")"
+        ).execute(&mut conn).await?;
+
+    let mut messages = sqlx::query(
+        "SELECT * FROM messages;"
+        ).fetch(&mut conn);
+
+    // while let Some(message) = messages.try_next().await? {
+    //     println!("{:?}", message);
+    // }
+
+    Ok(())
 }
