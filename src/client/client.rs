48c2d9425c5c319289697a818d3292da39149b0f
muoscar <oscar.muhr@gmail.com>
2024-06-22 11:23:36 +0200

    feat: simple CLI client which allows the sending och messages and getting of message history

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use serde::Deserialize;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    /// Message to be sent to the server
+    #[arg(short, long)]
+    msg: Option<String>,
+
+    #[arg(long, short, action=clap::ArgAction::SetFalse)]
+    /// Flag whether to get and print chat history
+    should_print_history: bool,
+}
+
+#[derive(Debug, Deserialize)]
+struct Msg {
+    id: u32,
+    message: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    if let Some(msg) = args.msg {
+        println!("Sending message:\n\t{msg}");
+
+        let client = reqwest::Client::new();
+        let res = client
+            .post("http://127.0.0.0:32123")
+            .body(msg)
+            .send()
+            .await?;
+        println!("Message sent, received response:\n\t{res:?}")
+    };
+
+    if args.should_print_history {
+        let res: Vec<Msg> = reqwest::get("http://127.0.0.0:32123").await?.json().await?;
+        println!("Message history:");
+        for msg in res {
+            println!("Message {}: {}", msg.id, msg.message)
+        }
+    }
+
+    Ok(())
 }
